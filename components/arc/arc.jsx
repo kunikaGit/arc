55705f2565143ef1f6bec69260f46e1b54edddb5
kunikaesp <kunika@espsofttech.com>
2025-02-23 18:22:01 +0530

    updated

--- components/arc/arc.jsx
+++ components/arc/arc.jsx
@@ -1,9 +1,9 @@
 "use client"; // ✅ Required for GSAP
 
-import { useEffect, useRef } from "react";
-import gsap from "gsap-trial/dist/gsap";
-import { ScrollTrigger } from "gsap-trial/dist/ScrollTrigger";
-import { ScrollSmoother } from "gsap-trial/dist/ScrollSmoother";
+// import { useEffect, useRef } from "react";
+// import gsap from "gsap-trial/dist/gsap";
+// import { ScrollTrigger } from "gsap-trial/dist/ScrollTrigger";
+// import { ScrollSmoother } from "gsap-trial/dist/ScrollSmoother";
 import Hero from "@/components/homes/arc-home/Hero";
 import WhyChoose from "../homes/arc-home/whychoose";
 import Testimonials from "../homes/arc-home/testimonial";
@@ -13,42 +13,42 @@ import Footer1 from "../footer/Footer1";
 import CoverFlowSlider from "../homes/arc-home/CoverFlowSlider";
 
 export default function ArcContent() {
-    const mainRef = useRef(null);
-    const smootherRef = useRef(null);
+    // const mainRef = useRef(null);
+    // const smootherRef = useRef(null);
 
-    useEffect(() => {
-        gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+    // useEffect(() => {
+    //     gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-        smootherRef.current = ScrollSmoother.create({
-            wrapper: "#smooth-wrapper",
-            content: "#smooth-content",
-            smooth: 2,
-            effects: true,
-        });
+    //     smootherRef.current = ScrollSmoother.create({
+    //         wrapper: "#smooth-wrapper",
+    //         content: "#smooth-content",
+    //         smooth: 2,
+    //         effects: true,
+    //     });
 
-        ScrollTrigger.create({
-            trigger: ".hotbids-section",
-            start: "top center",
-            end: "+=300",
-            pin: true,
-            markers: false, // Remove markers in production
-        });
+    //     ScrollTrigger.create({
+    //         trigger: ".hotbids-section",
+    //         start: "top center",
+    //         end: "+=300",
+    //         pin: true,
+    //         markers: false, // Remove markers in production
+    //     });
 
-        return () => {
-            ScrollTrigger.getAll().forEach((trigger) => trigger.kill())
-        };
-    }, []);
+    //     return () => {
+    //         ScrollTrigger.getAll().forEach((trigger) => trigger.kill())
+    //     };
+    // }, []);
 
     return (
         <>
-                    <Header3/>
-            <div id="smooth-wrapper" ref={mainRef}>
+            <Header3 />
+            <div id="smooth-wrapper" >
                 <div id="smooth-content">
                     <main>
                         <Hero />
                         <div className="hotbids-section" data-speed="1.2">
-                               <CoverFlowSlider />
-                            
+                            <CoverFlowSlider />
+
                         </div>
                         <div className="hotbids-section" data-speed="1.5">
                             <WhyChoose />
